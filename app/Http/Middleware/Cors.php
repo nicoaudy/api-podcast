d4a14fa3231ac4940b440cd992b286bbd9e6fade
nicoaudy <nicoaudy1994@gmail.com>
2017-09-22 17:13:51 +0700

    just added cors middleware

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cors
+{
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        $response
+            ->header('Access-Control-Allow-Origin', env('CLIENT_URL'))
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+
+        return $response;
+    }
+}
