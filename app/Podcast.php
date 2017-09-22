c221b627710139a1d835ae0e1f67f79f9f498660
nicoaudy <nicoaudy1994@gmail.com>
2017-09-22 17:04:17 +0700

    change files transform using model in methods

--- app/Podcast.php
+++ app/Podcast.php
@@ -12,4 +12,12 @@ class Podcast extends Model
         'file_mp3',
         'file_ogg'
     ];
+
+    public function getFilesAttribute()
+    {
+        return collect([
+            'mp3' => $this->file_mp3,
+            'ogg' => $this->file_ogg,
+        ]);
+    }
 }
--- app/Transformers/PodcastTransformer.php
+++ app/Transformers/PodcastTransformer.php
@@ -13,10 +13,7 @@ class PodcastTransformer extends TransformerAbstract
             'id'                => $podcast->id,
             'title'             => $podcast->title,
             'description'       => $podcast->description,
-            'files'             => [
-                'mp3' => $podcast->file_mp3,
-                'ogg' => $podcast->file_ogg,
-            ],
+            'files'             => $podcast->files,
             'created_at'        => $podcast->created_at->toDateTimeString(),
             'created_at_human'  => $podcast->created_at->diffForHumans(),
         ];
