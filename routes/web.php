d4ffdf47b83720a612d787adfb02f2afc0dbb481
nicoaudy <nicoaudy1994@gmail.com>
2017-09-22 16:52:30 +0700

    fix route issue in nginx and register fractal service provider

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ $router->get('/key', function () {
     return str_random(32);
 });
 
-$router->get('/podcasts', 'PodcastController@index');
+$router->get('/all_podcasts', 'PodcastController@index');
 $router->get('/podcasts/{id}', 'PodcastController@show');
